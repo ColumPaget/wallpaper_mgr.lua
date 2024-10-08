781cb559041cef7257c83436eb5fc9d0abe661a3
Colum Paget <colum.paget@axiomgb.com>
2024-10-08 11:27:16 +0100

    v2.8

---
+++ wallpaperscraft.lua
@@ -0,0 +1,80 @@
+
+function InitWallpapersCraft()
+local mod={}
+
+mod.pages={}
+
+
+mod.get_image=function(self, page)
+local S, html, XML, tag 
+local url=""
+local title=""
+
+if strutil.strlen(page) == 0 then return nil end
+S=stream.STREAM(page, "")
+if S ~= nil
+then
+	html=S:readdoc()
+	XML=xml.XML(html)
+	S:close()
+
+	tag=XML:next()
+	while tag ~= nil
+	do
+  	  if tag.type == "img"
+	  then
+	    str=HtmlTagExtractHRef(tag.data, 'class="wallpaper__image"', "src=") 
+	    if strutil.strlen(str) > 0 then url=str end
+	  elseif tag.type == "title"
+	  then
+	    tag=XML:next()
+	    title=tag.data
+	  end
+	tag=XML:next()
+	end
+end
+
+return url, title
+end
+
+
+mod.get=function(self, source)
+local S, html, str, XML, category, len
+
+category=string.sub(source, 17)
+
+str=string.format("https://wallpaperscraft.com/catalog/%s/1920x1080/page%d", category, math.random(100))
+print(str)
+S=stream.STREAM(str, "")
+
+if S == nil or S:getvalue("HTTP:ResponseCode") ~= "200"
+then
+str=string.format("https://wallpaperscraft.com/catalog/%s/1920x1080", category)
+S=stream.STREAM(str, "")
+end
+
+if S ~= nil
+then
+	html=S:readdoc()
+	XML=xml.XML(html)
+	S:close()
+
+	tag=XML:next()
+	while tag ~= nil
+	do
+	if tag.type == "a" and string.sub(tag.data, 1, 24) == 'class="wallpapers__link"'
+	then
+		
+		str=HtmlTagExtractHRef(tag.data,"")
+		if str ~= nil then table.insert(self.pages, str) end
+	end
+	tag=XML:next()
+	end
+end
+
+str=SelectRandomItem(self.pages)
+return self:get_image("https://wallpaperscraft.com/" .. str)
+end
+
+return mod
+end
